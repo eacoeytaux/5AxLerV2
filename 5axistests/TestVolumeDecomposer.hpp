db7648a0cfdf05dec201998e40ebbb56983216fd
Hugh <howhelan@wpi.edu>
2017-02-19 22:26:26 -0500

    Bug fixes in seperate mesh function

---
+++ 5axistests/TestVolumeDecomposer.hpp
@@ -0,0 +1,36 @@
+//
+//  TestVolumeDecomposer.hpp
+//  5Axler_cura
+//
+//  Created by Hugh Whelan on 2/16/17.
+//  Copyright © 2017 Hugh Whelan. All rights reserved.
+//
+
+#ifndef TestVolumeDecomposer_hpp
+#define TestVolumeDecomposer_hpp
+
+#include <stdio.h>
+#include "../cppunit/TestFixture.h"
+#include "../src/settings/settings.h"
+#include "../src/MeshGroup.h"
+
+namespace cura{
+	
+class TestVolumeDecomposer : public CppUnit::TestFixture{
+public:
+	/*!
+	 * Set up test suite.  Creates an instance of a VolumeDecomposer.
+	 */
+	void setUp();
+	
+	/*!
+	 * Destroys the VolumeDecomposer
+	 */
+	void tearDown();
+	
+	//The actual test cases.
+	void validateSeperatedMeshes();
+};
+
+}
+#endif /* TestVolumeDecomposer_hpp */
---
+++ 5axistests/main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  5Axler_cura
+//
+//  Created by Hugh Whelan on 2/19/17.
+//  Copyright © 2017 Hugh Whelan. All rights reserved.
+//
+
+#include "main.hpp"
