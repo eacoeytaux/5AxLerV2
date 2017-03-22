c9498c1b2b3e7728a0862a191880c9a98b857847
Hugh <howhelan@wpi.edu>
2017-03-22 15:50:12 -0400

    merge

--- run.sh
+++ run.sh
@@ -9,16 +9,19 @@ parser.add_argument("--coverage", help="open html coverage report", action='stor
 args = parser.parse_args()
 
 #set up
-os.chdir("./build/CMakeFiles/_CuraEngine.dir/src/5axis/")
+os.chdir("./build/CMakeFiles/_CuraEngine.dir/src/")
+subprocess.call("rm *.gcda", shell=True)
+subprocess.call("rm *.gcov", shell=True)
+os.chdir("./5axis/")
 subprocess.call("rm *.gcda", shell=True)
 subprocess.call("rm *.gcov", shell=True)
 
 os.chdir("../../../../..")
 
 #run tests
-subprocess.call('./build/CuraEngine slice -v -j ./5axistests/fdmprinter.def.json -o "test.gcode" -e0 -l "./5axistests/simple.STL"', shell=True)
-subprocess.call('./build/CuraEngine slice -v -j ./5axistests/fdmprinter.def.json -o "test.gcode" -e0 -l "./5axistests/radial_overhang.STL"', shell=True)
-subprocess.call('./build/CuraEngine slice -v -j ./5axistests/fdmprinter.def.json -o "test.gcode" -e0 -l "./5axistests/double_overhang.STL"', shell=True)
+#subprocess.call('./build/CuraEngine slice -v -j ./5axistests/fdmprinter.def.json -o "test.gcode" -e0 -l "./5axistests/simple.STL"', shell=True)
+#subprocess.call('./build/CuraEngine slice -v -j ./5axistests/fdmprinter.def.json -o "test.gcode" -e0 -l "./5axistests/radial_overhang.STL"', shell=True)
+subprocess.call('./build/CuraEngine slice -v -j ./5axistests/fdmprinter.def.json -o "test.gcode" -e0 -l "./5axistests/tee.STL"', shell=True)
 
 if args.coverage:
 	#generate HMTL coverage file
